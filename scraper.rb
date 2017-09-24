57c30dbd3b372fc34eace1c12f4f2be0e39b90c7
Matthew Landauer <matthew@today.design>
2017-09-24 15:06:47 +1000

    Extract method

--- scraper.rb
+++ scraper.rb
@@ -4,27 +4,36 @@
 require 'scraperwiki'
 require 'mechanize'
 
-page = 1
-agent = Mechanize.new
+def campsite_pages
+  page = 1
+  agent = Mechanize.new
 
-loop do
-  # Read in the index page
-  url = 'http://www.nationalparks.nsw.gov.au/search/CampingAndAccommodation/' \
-        'LoadMore?LoadMoreCategory=Campgrounds&LoadMoreByPrimaryIdentity=0&' \
-        "LoadMorePageNumber=#{page}&LoadMoreDisplayText=Campgrounds&" \
-        'PageSize=10&'
-  doc = agent.get(url)
+  loop do
+    # Read in the index page
+    url = 'http://www.nationalparks.nsw.gov.au/' \
+          'search/CampingAndAccommodation/LoadMore?' \
+          'LoadMoreCategory=Campgrounds&' \
+          'LoadMoreByPrimaryIdentity=0&' \
+          "LoadMorePageNumber=#{page}&" \
+          'LoadMoreDisplayText=Campgrounds&' \
+          'PageSize=10&'
+    doc = agent.get(url)
 
-  doc.search('article').each do |article|
-    detail_url = article.at('h3 a')['href']
-    p detail_url
-    record = {
-      'detail_url' => detail_url
-    }
-    ScraperWiki.save_sqlite(['detail_url'], record)
+    doc.search('article').each do |article|
+      detail_url = article.at('h3 a')['href']
+      yield detail_url
+    end
+
+    # Check if the "more" link is there
+    break if doc.at('a.showMore').nil?
+    page += 1
   end
+end
 
-  # Check if the "more" link is there
-  break if doc.at('a.showMore').nil?
-  page += 1
+campsite_pages do |detail_url|
+  p detail_url
+  record = {
+    'detail_url' => detail_url
+  }
+  ScraperWiki.save_sqlite(['detail_url'], record)
 end
