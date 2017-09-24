9b6021e087c11ade7283cc669e8b1748ea473c1e
Matthew Landauer <matthew@today.design>
2017-09-24 15:00:16 +1000

    Get all the campsite urls

--- scraper.rb
+++ scraper.rb
@@ -7,20 +7,24 @@ require 'mechanize'
 page = 1
 agent = Mechanize.new
 
-# Read in the index page
-url = 'http://www.nationalparks.nsw.gov.au/search/CampingAndAccommodation/' \
-      'LoadMore?LoadMoreCategory=Campgrounds&LoadMoreByPrimaryIdentity=0&' \
-      "LoadMorePageNumber=#{page}&LoadMoreDisplayText=Campgrounds&PageSize=10&"
-page = agent.get(url)
+loop do
+  # Read in the index page
+  url = 'http://www.nationalparks.nsw.gov.au/search/CampingAndAccommodation/' \
+        'LoadMore?LoadMoreCategory=Campgrounds&LoadMoreByPrimaryIdentity=0&' \
+        "LoadMorePageNumber=#{page}&LoadMoreDisplayText=Campgrounds&" \
+        'PageSize=10&'
+  doc = agent.get(url)
 
-page.search('article').each do |article|
-  detail_url = article.at('h3 a')['href']
-  #p detail_url
-  record = {
-    'detail_url' => detail_url
-  }
-  ScraperWiki.save_sqlite(['detail_url'], record)
+  doc.search('article').each do |article|
+    detail_url = article.at('h3 a')['href']
+    p detail_url
+    record = {
+      'detail_url' => detail_url
+    }
+    ScraperWiki.save_sqlite(['detail_url'], record)
+  end
+
+  # Check if the "more" link is there
+  break if doc.at('a.showMore').nil?
+  page += 1
 end
-
-# Check if the "more" link is there
-p page.at('a.showMore')
