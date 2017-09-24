dca0b98f70bc2e36dadf96408baa23fbfb13c0d5
Matthew Landauer <matthew@today.design>
2017-09-24 14:50:14 +1000

    Get first page of urls

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,26 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env ruby
+# frozen_string_literal: true
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'scraperwiki'
+require 'mechanize'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+page = 1
+agent = Mechanize.new
+
+# Read in the index page
+url = 'http://www.nationalparks.nsw.gov.au/search/CampingAndAccommodation/' \
+      'LoadMore?LoadMoreCategory=Campgrounds&LoadMoreByPrimaryIdentity=0&' \
+      "LoadMorePageNumber=#{page}&LoadMoreDisplayText=Campgrounds&PageSize=10&"
+page = agent.get(url)
+
+page.search('article').each do |article|
+  detail_url = article.at('h3 a')['href']
+  #p detail_url
+  record = {
+    'detail_url' => detail_url
+  }
+  ScraperWiki.save_sqlite(['detail_url'], record)
+end
+
+# Check if the "more" link is there
+p page.at('a.showMore')
