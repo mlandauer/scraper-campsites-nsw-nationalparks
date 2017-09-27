42f93dc98d3e5627283ee19de65b7d2ba9d4008b
Matthew Landauer <matthew@today.design>
2017-09-28 02:52:07 +1000

    Processes facilities

--- scraper.rb
+++ scraper.rb
@@ -118,6 +118,21 @@ CAMPING_TYPES = [
   'Tent sites short walk from car park'
 ].freeze
 
+FACILITIES = [
+  'barbecue facilities',
+  'drinking water',
+  'picnic tables',
+  'showers',
+  'toilets',
+  'amenities block',
+  'boat ramp',
+  'cafe/kiosk',
+  'carpark',
+  'electric power',
+  'public phone',
+  'wireless internet'
+].freeze
+
 # Format the data differently
 ScraperWiki.select('* from data').each do |campsite|
   # data = JSON.parse(campsite['data'])
@@ -129,6 +144,20 @@ ScraperWiki.select('* from data').each do |campsite|
   # campsite['opening_times'] = data['Opening times']
   # campsite['please_note'] = data['Please note']
   # campsite['price'] = data['Price']
+
+  facilities = if campsite['facilities']
+                 campsite['facilities'].split(',').map(&:strip).map(&:downcase)
+               else
+                 []
+               end
+  facilities.each do |facility|
+    raise "Unexpected facility: #{facility}" unless FACILITIES.include? facility
+  end
+  campsite['barbecues'] = facilities.include?('barbecue facilities')
+  campsite['drinking_water'] = facilities.include?('drinking water')
+  campsite['picnic_tables'] = facilities.include?('picnic tables')
+  campsite['showers'] = facilities.include?('showers')
+  campsite['toilets'] = facilities.include?('toilets')
 
   # camping_types = campsite['camping_type'].split(',').map(&:strip)
   # camping_types.each do |type|
@@ -146,5 +175,5 @@ ScraperWiki.select('* from data').each do |campsite|
   #   camping_types.include?('Caravan site') ||
   #   camping_types.include?('High clearance caravan site')
   #
-  # ScraperWiki.save_sqlite(['id'], campsite)
+  ScraperWiki.save_sqlite(['id'], campsite)
 end
