8a7d5795dccc8640e400960f1436e9bd02a2c57b
Matthew Landauer <matthew@today.design>
2017-09-28 03:19:45 +1000

    Do everything at once now

--- scraper.rb
+++ scraper.rb
@@ -6,105 +6,6 @@ require 'mechanize'
 require 'json'
 require 'active_support/core_ext/string/filters'
 
-def campsite_pages
-  page = 1
-  agent = Mechanize.new
-
-  loop do
-    # Read in the index page
-    url = 'http://www.nationalparks.nsw.gov.au/' \
-          'search/CampingAndAccommodation/LoadMore?' \
-          'LoadMoreCategory=Campgrounds&' \
-          'LoadMoreByPrimaryIdentity=0&' \
-          "LoadMorePageNumber=#{page}&" \
-          'LoadMoreDisplayText=Campgrounds&' \
-          'PageSize=10&'
-    doc = agent.get(url)
-
-    doc.search('article').each do |article|
-      detail_url = article.at('h3 a')['href']
-      yield detail_url
-    end
-
-    # Check if the "more" link is there
-    break if doc.at('a.showMore').nil?
-    page += 1
-  end
-end
-
-agent = Mechanize.new
-
-# campsite_pages do |detail_url|
-#   doc = agent.get(detail_url)
-#   puts doc.body
-#   exit
-#
-#   p detail_url
-#   record = {
-#     'detail_url' => detail_url
-#   }
-#   ScraperWiki.save_sqlite(['detail_url'], record)
-# end
-
-# Campsite schema that we're aiming for:
-# "park-name": "blah",
-# "name": "Acacia Flat",
-# "description": "Explore the \"cradle of conservation\", the Blue Gum Forest. Enjoy birdwatching, long walks and plenty of photogenic flora.",
-# "position-lat": -33.6149,
-# "position-lng": 150.3553,
-# "facilities-toilets": "non_flush",
-# "facilities-picnicTables": false,
-# "facilities-barbecues": "wood",
-# "facilities-showers": "none",
-# "facilities-drinkingWater": false,
-# "access-caravans": false,
-# "access-trailers": false,
-# "access-car": false
-
-# Get campsite location data
-# doc = agent.get('http://www.nationalparks.nsw.gov.au/data/Map/GetPins')
-# campsites = JSON.parse(doc.body).select { |d| d['type'] == 'camping' }
-#
-# campsites.each do |campsite|
-#   id = campsite['id']
-#   title = campsite['title']
-#   puts title
-#   latitude = campsite['coords']['lat']
-#   longitude = campsite['coords']['lon']
-#   # Get some more detailed information about the campsite
-#   doc = agent.get(
-#     "http://www.nationalparks.nsw.gov.au/data/Map/GetItem?id=#{id}"
-#   )
-#   data = JSON.parse(doc.body)
-#   url = data['Url']
-#   park_name = data['WhereText']
-#   description = data['ShortDescription']
-#   booking_url = data['BookingURL']['Url'] if data['BookingURL']
-#
-#   # Get the final details from the human readable campsite detail page
-#   doc = agent.get(url)
-#   # Extract table info
-#   data = {}
-#   doc.at('table.itemDetails').search('tr').each do |tr|
-#     th = tr.at('th').inner_html.squish
-#     td = tr.at('td').inner_html.squish
-#     data[th] = td
-#   end
-#
-#   record = {
-#     'title' => title,
-#     'latitude' => latitude,
-#     'longitude' => longitude,
-#     'id' => id,
-#     'url' => url,
-#     'park_name' => park_name,
-#     'description' => description,
-#     'booking_url' => booking_url,
-#     'data' => data.to_json
-#   }
-#   ScraperWiki.save_sqlite(['id'], record)
-# end
-
 CAMPING_TYPES = [
   'Camping beside my vehicle',
   'Camping beside my vehicle Short walk from parking',
@@ -144,11 +45,11 @@ def parse_facilities(text)
     raise "Unexpected facility: #{facility}" unless FACILITIES.include? facility
   end
   {
-    'barbecues' => facilities.include?('barbecue facilities'),
-    'drinking_water' => facilities.include?('drinking water'),
-    'picnic_tables' => facilities.include?('picnic tables'),
-    'showers' => facilities.include?('showers'),
-    'toilets' => facilities.include?('toilets')
+    'barbecues' => facilities.include?('barbecue facilities').to_s,
+    'drinking_water' => facilities.include?('drinking water').to_s,
+    'picnic_tables' => facilities.include?('picnic tables').to_s,
+    'showers' => facilities.include?('showers').to_s,
+    'toilets' => facilities.include?('toilets').to_s
   }
 end
 
@@ -170,28 +71,64 @@ def parse_camping_type(text)
     camping_types.include?('High clearance caravan site')
 
   {
-    'car' => car,
-    'trailers' => trailers,
-    'caravans' => caravans
+    'car' => car.to_s,
+    'trailers' => trailers.to_s,
+    'caravans' => caravans.to_s
   }
 end
 
-# Format the data differently
-ScraperWiki.select('* from data').each do |campsite|
-  # data = JSON.parse(campsite['data'])
-  # campsite['bookings'] = data['Bookings']
-  # campsite['camping_type'] = data['Camping type']
-  # campsite['entry_fees'] = data['Entry fees']
-  # campsite['facilities'] = data['Facilities']
-  # campsite['no_of_campsites'] = data['Number of campsites']
-  # campsite['opening_times'] = data['Opening times']
-  # campsite['please_note'] = data['Please note']
-  # campsite['price'] = data['Price']
+agent = Mechanize.new
 
-  campsite = campsite
-             .merge(parse_facilities(campsite['facilities']))
-             .merge(parse_camping_type(campsite['camping_type']))
+# Get campsite location data
+doc = agent.get('http://www.nationalparks.nsw.gov.au/data/Map/GetPins')
+campsites = JSON.parse(doc.body).select { |d| d['type'] == 'camping' }
 
-  p campsite
-  # ScraperWiki.save_sqlite(['id'], campsite)
+campsites.each do |campsite|
+  id = campsite['id']
+  title = campsite['title']
+  puts title
+  latitude = campsite['coords']['lat']
+  longitude = campsite['coords']['lon']
+  # Get some more detailed information about the campsite
+  doc = agent.get(
+    "http://www.nationalparks.nsw.gov.au/data/Map/GetItem?id=#{id}"
+  )
+  data = JSON.parse(doc.body)
+  url = data['Url']
+  park_name = data['WhereText']
+  description = data['ShortDescription']
+  booking_url = data['BookingURL']['Url'] if data['BookingURL']
+
+  # Get the final details from the human readable campsite detail page
+  doc = agent.get(url)
+  # Extract table info
+  data = {}
+  doc.at('table.itemDetails').search('tr').each do |tr|
+    th = tr.at('th').inner_html.squish
+    td = tr.at('td').inner_html.squish
+    data[th] = td
+  end
+
+  record = {
+    'title' => title,
+    'latitude' => latitude,
+    'longitude' => longitude,
+    'id' => id,
+    'url' => url,
+    'park_name' => park_name,
+    'description' => description,
+    'booking_url' => booking_url,
+    'bookings' => data['Bookings'],
+    'entry_fees' => data['Entry fees'],
+    'no_of_campsites' => data['Number of campsites'],
+    'opening_times' => data['Opening times'],
+    'please_note' => data['Please note'],
+    'price' => data['Price']
+  }
+
+  record = record
+           .merge(parse_facilities(data['Facilities']))
+           .merge(parse_camping_type(data['Camping type']))
+
+  ScraperWiki.save_sqlite(['id'], record)
 end
